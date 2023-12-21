a8ad5cc26dce0a937625442ffa12a383119ec39c
IvoVM <ivomastrangelo12@gmail.com>
2023-12-21 19:15:34 -0300

    authService created

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment.prod';
+import { User } from '../types/user.type';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+  checkEmailInUse(email: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    const params = new HttpParams().set('email', email);
+    return this.http.get(environment.apiUrl + '/api/Users/ValidateEmail', {
+      headers,
+      params,
+    });
+  }
+
+  register(body: User): Observable<any> {
+    return this.http.post(environment.apiUrl + '/api/Users/Register', body);
+  }
+}
