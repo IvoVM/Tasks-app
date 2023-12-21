fcb5b166aece65fa1ffdd82c5f5a993f44d874b6
IvoVM <ivomastrangelo12@gmail.com>
2023-12-21 17:26:00 -0300

    Created authGuard

---
+++ src/app/Guards/auth.guard.ts
@@ -0,0 +1,13 @@
+import { Inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = Inject(Router);
+  const isAuthenticated = false;
+
+  if (isAuthenticated) {
+    return true;
+  }
+  router.navigateByUrl('login');
+  return false;
+};
