b7a027844183bc0ad0cfac4162ee59f84d1e67fa
IvoVM <ivomastrangelo12@gmail.com>
2023-12-22 16:56:59 -0300

    Header now only shows user menu if user is logged in, added logout dialog

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { UserResponse } from 'src/app/types/user.type';
+import { MatDialog } from '@angular/material/dialog';
+import { LogoutComponent } from '../logout/logout.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  public user!: UserResponse | null;
+  constructor(private userSvc: UserService, private dialog: MatDialog) {}
+  ngOnInit(): void {
+    this.userSvc.getUser().subscribe((user) => {
+      this.user = user;
+    });
+  }
 
+  openDialog(): void {
+    this.dialog.open(LogoutComponent, {
+      data: { userEmail: this.user?.email },
+    });
+  }
 }
