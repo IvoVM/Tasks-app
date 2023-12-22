5cbac5d923f290ece0ceba241d1668ee243a9b0c
IvoVM <ivomastrangelo12@gmail.com>
2023-12-22 18:45:38 -0300

    Manage categories data receivement, changed default inputValue

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Categories } from 'src/app/types/task.type';
 
 @Component({
   selector: 'app-task-form',
@@ -6,11 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-form.component.scss'],
 })
 export class TaskFormComponent {
-  inputValue = 0;
+  inputValue = 1;
+  categories: Categories[] = [];
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { categories: Categories[] }
+  ) {
+    // Inicializar las categorías con los datos proporcionados
+    if (data && data.categories) {
+      this.categories = data.categories;
+    }
+  }
 
   onInputChange(event: any) {
     const target = event.target;
-    this.inputValue = parseInt(target.value, 10) || 0;
+    this.inputValue = parseInt(target.value, 10) || 1;
   }
 
   onCounterChange(value: number) {
