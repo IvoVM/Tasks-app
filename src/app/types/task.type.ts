f3cf8bcd096bb09025243c0f0cbbff2c86767899
IvoVM <ivomastrangelo12@gmail.com>
2023-12-22 16:54:46 -0300

    Tasks' types created

---
+++ src/app/types/task.type.ts
@@ -0,0 +1,24 @@
+export interface Task {
+  title: string;
+  description: string;
+  priority: number;
+  category_id: number;
+}
+
+export interface TaskResponse {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  priority: number;
+  is_completed: boolean;
+  last_update_date: string;
+}
+
+export interface TaskPage {
+  current_page: number;
+  page_size: number;
+  total_items: number;
+  total_pages: number;
+  items: TaskResponse[];
+}
