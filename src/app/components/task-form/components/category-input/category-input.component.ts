a2f5bdfafe8a3861b0fc1fdc7f621f981e999248
IvoVM <ivomastrangelo12@gmail.com>
2023-12-22 18:38:58 -0300

    Finished caregory-input component

--- src/app/components/task-form/components/category-input/category-input.component.ts
+++ src/app/components/task-form/components/category-input/category-input.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Categories } from 'src/app/types/task.type';
 
 @Component({
   selector: 'app-category-input',
   templateUrl: './category-input.component.html',
-  styleUrls: ['./category-input.component.scss']
+  styleUrls: ['./category-input.component.scss'],
 })
 export class CategoryInputComponent {
+  @Input() categories: Categories[] = [];
+  @Output() categorySelected = new EventEmitter<number>();
+  selectedCategoryId: number | undefined;
 
+  onSelectChange() {
+    if (this.selectedCategoryId !== undefined) {
+      this.categorySelected.emit(this.selectedCategoryId);
+    }
+  }
 }
