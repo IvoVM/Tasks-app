e688dbaa606cee6ccac69291480bb29b11aca18e
IvoVM <ivomastrangelo12@gmail.com>
2023-12-22 16:54:15 -0300

    Created average http petitions to manage tasks

---
+++ src/app/services/tasks.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment.prod';
+import { Task, TaskPage, TaskResponse } from '../types/task.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TasksService {
+  private apiUrl = environment.apiUrl + '/Tasks';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<TaskPage> {
+    return this.http.get<TaskPage>(this.apiUrl);
+  }
+
+  getTaskById(id: number): Observable<Task> {
+    return this.http.get<Task>(`${this.apiUrl}/${id}`);
+  }
+
+  createTask(task: Task): Observable<TaskResponse> {
+    return this.http.post<TaskResponse>(this.apiUrl, task);
+  }
+
+  updateTask(id: number, task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${id}`, task);
+  }
+
+  deleteTask(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
