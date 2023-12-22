f5b271ffd98cf4ba38aa59cb79f811c5774985fd
IvoVM <ivomastrangelo12@gmail.com>
2023-12-22 18:47:00 -0300

    Implemented form in add new task button

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskFormComponent } from '../task-form/task-form.component';
+import { TasksService } from 'src/app/services/tasks.service';
+import { MatDialog } from '@angular/material/dialog';
+import { Categories } from 'src/app/types/task.type';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  categories!: Categories[];
+  constructor(private taskSvc: TasksService, private dialog: MatDialog) {}
+  ngOnInit(): void {
+    this.taskSvc.getCategories().subscribe({
+      next: (res) => {
+        this.categories = res;
+        console.log(this.categories);
+      },
+      error: (err) => {
+        console.log('error fetching categories', err);
+      },
+    });
+  }
 
+  openForm(): void {
+    if (this.categories) {
+      this.dialog.open(TaskFormComponent, {
+        data: { categories: this.categories },
+      });
+    } else {
+      console.log('form is not available');
+    }
+  }
 }
