9bac859e967295245c9ea9dd088277ccfa097754
IvoVM <ivomastrangelo12@gmail.com>
2023-12-23 19:20:13 -0300

    Fixed some functions

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -15,29 +15,35 @@ export class TasksService {
   getTasks(
     page_size: number,
     page_number: number,
-    completed: boolean
+    completed: boolean = false
   ): Observable<TaskPage> {
     const params = {
       page_size: page_size.toString(),
       page_number: page_number.toString(),
-      completed: completed.toString(),
+      completed: completed,
     };
 
     return this.http.get<TaskPage>(this.apiUrl, { params });
   }
 
-  getTaskById(id: number): Observable<Task> {
-    return this.http.get<Task>(`${this.apiUrl}/${id}`);
+  getTaskById(id: string): Observable<TaskResponse> {
+    return this.http.get<TaskResponse>(`${this.apiUrl}/${id}`);
   }
 
   createTask(task: Task): Observable<TaskResponse> {
     return this.http.post<TaskResponse>(this.apiUrl, task);
   }
 
-  updateTask(id: number, task: Task): Observable<Task> {
+  updateTask(id: string, task: Task): Observable<Task> {
     return this.http.put<Task>(`${this.apiUrl}/${id}`, task);
   }
 
+  updateTaskStatus(body: {
+    id: number;
+    is_completed: boolean;
+  }): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}` + '/status', body);
+  }
   deleteTask(id: number): Observable<any> {
     return this.http.delete(`${this.apiUrl}/${id}`);
   }
