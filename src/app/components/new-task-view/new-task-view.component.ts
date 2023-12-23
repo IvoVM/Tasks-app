70ba3715eb37dd97c7b6d4f3c85222e704f977d4
IvoVM <ivomastrangelo12@gmail.com>
2023-12-23 19:22:26 -0300

    Component is now functional

--- src/app/components/new-task-view/new-task-view.component.ts
+++ src/app/components/new-task-view/new-task-view.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TasksService } from 'src/app/services/tasks.service';
+import { Categories } from 'src/app/types/task.type';
 
 @Component({
   selector: 'app-new-task-view',
   templateUrl: './new-task-view.component.html',
-  styleUrls: ['./new-task-view.component.scss']
+  styleUrls: ['./new-task-view.component.scss'],
 })
-export class NewTaskViewComponent {
+export class NewTaskViewComponent implements OnInit {
+  categories: Categories[] = [];
 
+  constructor(private taskSvc: TasksService, private _snackBar: MatSnackBar) {}
+  ngOnInit(): void {
+    this.getFormCategories();
+  }
+  getFormCategories(): void {
+    this.taskSvc.getCategories().subscribe({
+      next: (res) => {
+        this.categories = res;
+      },
+      error: (err) => {
+        console.log('error fetching categories', err);
+      },
+    });
+  }
+
+  uploadNewTask(event: any) {
+    this.taskSvc.createTask(event).subscribe({
+      next: () => {
+        this.openSnackBar('Tarea subida con exito');
+      },
+      error: () => {
+        this.openSnackBar(
+          'Error al intentar subir la tarea, intentelo mas tarde'
+        );
+      },
+    });
+  }
+
+  openSnackBar(msg: string) {
+    this._snackBar.open(msg, 'Cerrar', {
+      duration: 4000,
+    });
+  }
 }
