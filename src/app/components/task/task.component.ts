2ecc3f4c039f9ce6e857696aec65de48598507b6
IvoVM <ivomastrangelo12@gmail.com>
2023-12-23 12:26:24 -0300

    Added functionality to checkbox

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { TasksService } from 'src/app/services/tasks.service';
 import { TaskResponse } from 'src/app/types/task.type';
 
 @Component({
@@ -8,4 +9,22 @@ import { TaskResponse } from 'src/app/types/task.type';
 })
 export class TaskComponent {
   @Input() data!: TaskResponse;
+
+  constructor(private taskService: TasksService) {}
+
+  onCheckboxChange($event: any) {
+    const is_completed = $event.target.checked;
+    let body = {
+      is_completed,
+      id: this.data.id,
+    };
+    this.taskService.updateTaskStatus(body).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
