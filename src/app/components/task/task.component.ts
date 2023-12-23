f840d87dca1b881c7f25cad6566f6a47f7dbf952
IvoVM <ivomastrangelo12@gmail.com>
2023-12-22 21:59:35 -0300

    implemented data input, pipes, minor change to scss

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TaskResponse } from 'src/app/types/task.type';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.scss']
+  styleUrls: ['./task.component.scss'],
 })
 export class TaskComponent {
-
+  @Input() data!: TaskResponse;
 }
