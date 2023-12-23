4a8658502378d8442374b19acf036ae43b77fce0
IvoVM <ivomastrangelo12@gmail.com>
2023-12-23 19:21:23 -0300

    Fixed some functions and types

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,12 +2,24 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment.prod';
-import { User } from '../types/user.type';
+import { User, UserLogin, UserResponse } from '../types/user.type';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
+
+  login(body: UserLogin): Observable<UserResponse> {
+    return this.http.post<UserResponse>(
+      environment.apiUrl + '/Users/Login',
+      body
+    );
+  }
+
+  register(body: User): Observable<any | null> {
+    return this.http.post(environment.apiUrl + '/Users/Register', body);
+  }
+
   checkEmailInUse(email: string): Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
@@ -17,9 +29,5 @@ export class AuthService {
       headers,
       params,
     });
-  }
-
-  register(body: User): Observable<any> {
-    return this.http.post(environment.apiUrl + '/Users/Register', body);
   }
 }
