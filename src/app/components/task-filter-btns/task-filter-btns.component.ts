6a701a7b2fdabc995b5584ec3b826735ee695ed3
IvoVM <ivomastrangelo12@gmail.com>
2023-12-23 12:25:52 -0300

    Task-filter butons implemented

---
+++ src/app/components/task-filter-btns/task-filter-btns.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-filter-btns',
+  templateUrl: './task-filter-btns.component.html',
+  styleUrls: ['./task-filter-btns.component.scss']
+})
+export class TaskFilterBtnsComponent {
+
+}
