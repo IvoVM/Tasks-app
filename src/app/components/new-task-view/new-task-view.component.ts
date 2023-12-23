134402cc3dbbe9b25a766baefda4db5493632973
IvoVM <ivomastrangelo12@gmail.com>
2023-12-23 18:38:09 -0300

    Created new task view, moved all component to create new task to this folder

---
+++ src/app/components/new-task-view/new-task-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task-view',
+  templateUrl: './new-task-view.component.html',
+  styleUrls: ['./new-task-view.component.scss']
+})
+export class NewTaskViewComponent {
+
+}
