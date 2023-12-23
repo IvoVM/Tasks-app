b27874f39ab611ee87ebb2aebd37443bd96ab42a
IvoVM <ivomastrangelo12@gmail.com>
2023-12-23 01:28:41 -0300

    Added userResponse type, edited user type

--- src/app/types/user.type.ts
+++ src/app/types/user.type.ts
@@ -1,6 +1,17 @@
-export interface User {
+export interface UserLogin {
   email: 'string';
-  first_name: 'string';
-  last_name: 'string';
   password: 'string';
 }
+export interface User extends UserLogin {
+  first_name: 'string';
+  last_name: 'string';
+}
+
+export type UserResponse = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  id: string;
+  access_token: string;
+  access_token_expiration: string;
+};
