dc7c8d3635352e4cdaadca5e0773809f6c4d99f7
IvoVM <ivomastrangelo12@gmail.com>
2023-12-22 21:58:54 -0300

    Created truncate-text pipe

---
+++ src/app/pipes/truncate-text.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncateText',
+})
+export class TruncateTextPipe implements PipeTransform {
+  transform(text: string, maxLength: number): string {
+    if (text.length <= maxLength) {
+      return text;
+    }
+
+    return text.substring(0, maxLength - 3) + '...';
+  }
+}
