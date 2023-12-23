801c48dbade1c8ecdcb48e40386c15c74616f390
IvoVM <ivomastrangelo12@gmail.com>
2023-12-23 19:23:05 -0300

    Added pointer class, deleted unnecesary code

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,36 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { TaskFormComponent } from '../task-form/task-form.component';
-import { TasksService } from 'src/app/services/tasks.service';
-import { MatDialog } from '@angular/material/dialog';
-import { Categories } from 'src/app/types/task.type';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  categories!: Categories[];
-  constructor(private taskSvc: TasksService, private dialog: MatDialog) {}
-  ngOnInit(): void {
-    this.taskSvc.getCategories().subscribe({
-      next: (res) => {
-        this.categories = res;
-        console.log(this.categories);
-      },
-      error: (err) => {
-        console.log('error fetching categories', err);
-      },
-    });
-  }
-
-  openForm(): void {
-    if (this.categories) {
-      this.dialog.open(TaskFormComponent, {
-        data: { categories: this.categories },
-      });
-    } else {
-      console.log('form is not available');
-    }
-  }
-}
+export class HomeComponent {}
