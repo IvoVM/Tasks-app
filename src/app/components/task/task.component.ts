aec5862dd256d7f852eca6efff2198afa6ebd6a6
IvoVM <ivomastrangelo12@gmail.com>
2023-12-23 21:18:19 -0300

    Trash icon opens modal, checkbox now changes taskArrayService's array

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { TasksService } from 'src/app/services/tasks.service';
 import { TaskResponse } from 'src/app/types/task.type';
+import { DeleteTaskModalComponent } from '../delete-task-modal/delete-task-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskArrayService } from 'src/app/shared/task-array.service';
 
 @Component({
   selector: 'app-task',
@@ -10,7 +13,11 @@ import { TaskResponse } from 'src/app/types/task.type';
 export class TaskComponent {
   @Input() data!: TaskResponse;
 
-  constructor(private taskService: TasksService) {}
+  constructor(
+    private taskService: TasksService,
+    private dialog: MatDialog,
+    private taskArraySvc: TaskArrayService
+  ) {}
 
   onCheckboxChange($event: any) {
     const is_completed = $event.target.checked;
@@ -18,13 +25,20 @@ export class TaskComponent {
       is_completed,
       id: this.data.id,
     };
+
+    this.taskArraySvc.toggleTaskCompletion(this.data.id);
+
     this.taskService.updateTaskStatus(body).subscribe({
-      next: (res) => {
-        console.log(res);
-      },
+      next: () => {},
       error: (err) => {
         console.log(err);
       },
+    });
+  }
+
+  openDeleteModal() {
+    this.dialog.open(DeleteTaskModalComponent, {
+      data: { data: this.data },
     });
   }
 }
