55f0c845695483896b215413ed8251e0d4244b54
IvoVM <ivomastrangelo12@gmail.com>
2023-12-24 14:31:06 -0300

    Created task-detail component

--- src/app/components/task-detail/task-detail.component.ts
+++ src/app/components/task-detail/task-detail.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TasksService } from './../../services/tasks.service';
+import { Component, OnInit } from '@angular/core';
+import { TaskResponse } from 'src/app/types/task.type';
 
 @Component({
   selector: 'app-task-detail',
   templateUrl: './task-detail.component.html',
-  styleUrls: ['./task-detail.component.scss']
+  styleUrls: ['./task-detail.component.scss'],
 })
-export class TaskDetailComponent {
+export class TaskDetailComponent implements OnInit {
+  data!: TaskResponse;
+  error!: string;
+  constructor(
+    private tasksService: TasksService,
+    private route: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const taskId = params['id'];
+      this.getTaskData(taskId);
+    });
+  }
 
+  getTaskData(taskId: string) {
+    this.tasksService.getTaskById(taskId).subscribe({
+      next: (res) => {
+        this.data = res;
+        this.error = '';
+      },
+      error: () => {
+        this.error = `Los datos para la tarea de id:${taskId} no pudieron ser recuperados, intentelo mas tarde`;
+      },
+    });
+  }
 }
