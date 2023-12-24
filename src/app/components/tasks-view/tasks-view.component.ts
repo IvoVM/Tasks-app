04e696f49cc43fae4f25fad30db3fdb55dc0fb52
IvoVM <ivomastrangelo12@gmail.com>
2023-12-23 21:03:19 -0300

    Tasks-view component is now conected to arrayService, therefore data changes are now shown on real time

--- src/app/components/tasks-view/tasks-view.component.ts
+++ src/app/components/tasks-view/tasks-view.component.ts
@@ -1,56 +1,80 @@
-import { TasksService } from 'src/app/services/tasks.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TaskResponse } from 'src/app/types/task.type';
+import { Subscription } from 'rxjs';
+import { TaskArrayService } from 'src/app/shared/task-array.service';
+import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
   selector: 'app-tasks-view',
   templateUrl: './tasks-view.component.html',
   styleUrls: ['./tasks-view.component.scss'],
 })
-export class TasksViewComponent implements OnInit {
+export class TasksViewComponent implements OnInit, OnDestroy {
   tasks!: TaskResponse[];
   error!: string;
   incompletedBtnSelected = true;
-  constructor(private tasksService: TasksService) {}
+
+  private tasksSubscription: Subscription = new Subscription();
+
+  constructor(
+    private taskArraySvc: TaskArrayService,
+    private taskSvc: TasksService
+  ) {}
+
   ngOnInit(): void {
+    this.subscribeToTasks();
     this.getIncompleteTasks();
   }
 
+  ngOnDestroy(): void {
+    this.tasksSubscription.unsubscribe();
+  }
+
+
+  //Se Subscribe a los cambios en el array para actualizar el listado de tasks.
+  subscribeToTasks() {
+    this.tasksSubscription = this.taskArraySvc.tasks$.subscribe((tasks) => {
+      if (this.incompletedBtnSelected) {
+        this.tasks = tasks.filter(task => !task.is_completed);
+      } else {
+        this.tasks = tasks.filter(task => task.is_completed);
+      }
+    });
+  }
+  
+
   getIncompleteTasks() {
     this.incompletedBtnSelected = true;
-    this.tasksService.getTasks(10, 1).subscribe({
+    this.taskSvc.getTasks(10, 1).subscribe({
       next: (res) => {
-        this.tasks = res.items;
+        this.taskArraySvc.updateTasks(res.items);
         if (!this.tasks.length) {
           this.error = 'No hay tareas incompletas';
         } else {
           this.error = '';
         }
-
       },
       error: () => {
         this.error =
-          ' Hubo un error cargando la data, por favor intentelo de nuevo mas tarde';
+          'Hubo un error cargando la data, por favor inténtelo de nuevo más tarde';
       },
     });
   }
 
   getCompleteTasks() {
     this.incompletedBtnSelected = false;
-
-    this.tasksService.getTasks(10, 1, true).subscribe({
+    this.taskSvc.getTasks(10, 1, true).subscribe({
       next: (res) => {
-        this.tasks = res.items;
+        this.taskArraySvc.updateTasks(res.items);
         if (!this.tasks.length) {
           this.error = 'No hay tareas completas';
         } else {
           this.error = '';
         }
-
       },
       error: () => {
         this.error =
-          ' Hubo un error cargando la data, por favor intentelo de nuevo mas tarde';
+          'Hubo un error cargando la data, por favor inténtelo de nuevo más tarde';
       },
     });
   }
