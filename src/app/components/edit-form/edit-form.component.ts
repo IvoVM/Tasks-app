697e7ffea4fe0c6850f83832b4fe3b435f8d605b
IvoVM <ivomastrangelo12@gmail.com>
2023-12-24 16:41:31 -0300

    Created input title

--- src/app/components/edit-form/edit-form.component.ts
+++ src/app/components/edit-form/edit-form.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { TasksService } from 'src/app/services/tasks.service';
+import { TaskArrayService } from 'src/app/shared/services/task-array.service';
+import { Categories } from 'src/app/types/task.type';
 
 @Component({
   selector: 'app-edit-form',
   templateUrl: './edit-form.component.html',
-  styleUrls: ['./edit-form.component.scss']
+  styleUrls: ['./edit-form.component.scss'],
 })
-export class EditFormComponent {
+export class EditFormComponent implements OnInit {
+  categories: Categories[] = [];
 
+  constructor(
+    private taskSvc: TasksService,
+    private _snackBar: MatSnackBar,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.getFormCategories();
+  }
+  getFormCategories(): void {
+    this.taskSvc.getCategories().subscribe({
+      next: (res) => {
+        this.categories = res;
+      },
+      error: (err) => {
+        console.log('error fetching categories', err);
+      },
+    });
+  }
+
+  editTask(event: any) {}
+
+  openSnackBar(msg: string) {
+    this._snackBar.open(msg, 'Cerrar', {
+      duration: 4000,
+    });
+  }
 }
