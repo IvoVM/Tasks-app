758f76b9192eca44fb19b468c8e54513aab5d878
IvoVM <ivomastrangelo12@gmail.com>
2023-12-24 14:13:29 -0300

    Html side of component is all on ts now

--- src/app/components/workspace-btn/workspace-btn.component.ts
+++ src/app/components/workspace-btn/workspace-btn.component.ts
@@ -2,7 +2,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-workspace-btn',
-  templateUrl: './workspace-btn.component.html',
+  template: `
+  <button
+    mat-button
+    [disabled]="disabled"
+    class="filter-btn"
+    [ngClass]="{
+      white: color === 'white',
+      blue: color === 'blue',
+      grey: color === 'grey'
+    }"
+  >
+    {{ text }}
+  </button> 
+  `,
   styleUrls: ['./workspace-btn.component.scss'],
 })
 export class WorkspaceBtnComponent {
@@ -10,10 +23,9 @@ export class WorkspaceBtnComponent {
   @Input() text: string = '';
   @Input() color: string = 'blue';
 
-
   @Output() buttonClick: EventEmitter<void> = new EventEmitter<void>();
 
-  get buttonClasses(): any {
+  get buttonClasses():any {
     return {
       'disabled-text': this.disabled,
       text: !this.disabled,
