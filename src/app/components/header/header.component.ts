4718b549042b2155caa1c7cc9f8a24577a0a6aaf
IvoVM <ivomastrangelo12@gmail.com>
2023-12-24 14:22:55 -0300

    Component subscribed to incompletedTasksLenght subject

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,9 @@ import { UserService } from 'src/app/services/user.service';
 import { UserResponse } from 'src/app/types/user.type';
 import { MatDialog } from '@angular/material/dialog';
 import { LogoutComponent } from '../logout/logout.component';
+import { TasksService } from 'src/app/services/tasks.service';
+import { Subscription } from 'rxjs';
+import { TaskArrayService } from 'src/app/shared/task-array.service';
 
 @Component({
   selector: 'app-header',
@@ -11,11 +14,48 @@ import { LogoutComponent } from '../logout/logout.component';
 })
 export class HeaderComponent implements OnInit {
   public user!: UserResponse | null;
-  constructor(private userSvc: UserService, private dialog: MatDialog) {}
+  private incompleteTasksSubscription: Subscription = new Subscription();
+  lengthNumber = 0;
+
+  constructor(
+    private userSvc: UserService,
+    private dialog: MatDialog,
+    private taskSvc: TasksService,
+    private taskArraySvc: TaskArrayService
+  ) {}
+
   ngOnInit(): void {
+    this.getUserData();
+    this.getIncompleteTaskLength();
+    this.subscribeToTasks();
+  }
+
+  getUserData() {
     this.userSvc.getUser().subscribe((user) => {
       this.user = user;
     });
+  }
+
+  getIncompleteTaskLength() {
+    this.taskSvc.getIncompleteTasksLenght().subscribe({
+      next: (res) => {
+        this.taskArraySvc.setFirstValue(res.incomplete_task_count);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  subscribeToTasks() {
+    this.incompleteTasksSubscription =
+      this.taskArraySvc.incompletedTasksLenght$.subscribe((value) => {
+        this.lengthNumber = value;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.incompleteTasksSubscription.unsubscribe();
   }
 
   openDialog(): void {
