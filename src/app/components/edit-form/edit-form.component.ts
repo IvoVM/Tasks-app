4b08c256e31b16eb5f5dba211d3c5bf5f163bd9f
IvoVM <ivomastrangelo12@gmail.com>
2023-12-25 15:40:35 -0300

    Edit-form now shows andhide spinner, also show a snackbar

--- src/app/components/edit-form/edit-form.component.ts
+++ src/app/components/edit-form/edit-form.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { TasksService } from 'src/app/services/tasks.service';
+import { SpinnerService } from 'src/app/shared/services/spinner.service';
 import { TaskArrayService } from 'src/app/shared/services/task-array.service';
 import { Categories, Task } from 'src/app/types/task.type';
 
@@ -9,18 +11,20 @@ import { Categories, Task } from 'src/app/types/task.type';
   selector: 'app-edit-form',
   templateUrl: './edit-form.component.html',
 })
-
-
-export class EditFormComponent implements OnInit {
+export class EditFormComponent implements OnInit, OnDestroy {
   categories: Categories[] = [];
   id = this.route.snapshot.paramMap.get('id');
   taskTitle = this.route.snapshot.paramMap.get('title');
+
+  spinnerSubscription: Subscription = new Subscription();
+  showSpinner = false;
 
   constructor(
     private taskSvc: TasksService,
     private _snackBar: MatSnackBar,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private spinnerService: SpinnerService
   ) {}
   ngOnInit(): void {
     this.getFormCategories();
@@ -29,23 +33,33 @@ export class EditFormComponent implements OnInit {
     this.taskSvc.getCategories().subscribe({
       next: (res) => {
         this.categories = res;
-        console.log(res);
       },
       error: (err) => {
-        console.log('error fetching categories', err);
+        console.error(err);
+        this.openSnackBar('Hubo un error intentelo mas tarde');
+      },
+      complete: () => {
+        this.spinnerService.hideSpinner();
       },
     });
   }
 
   editTask(event: Task) {
+    this.spinnerService.showSpinner();
     if (this.id) {
       this.taskSvc.updateTask(this.id, event).subscribe({
         next: () => {
+          this.openSnackBar('La tarea fue editada con exito');
           this.router.navigateByUrl('home');
-          console.log('Task updated');
         },
-        error: () => {
-          console.log('error updating');
+        error: (err) => {
+          console.log(err);
+          this.openSnackBar(
+            'Hubo un error al editar la tarea, intentelo mas tarde.'
+          );
+        },
+        complete: () => {
+          this.spinnerService.hideSpinner();
         },
       });
     }
@@ -55,5 +69,17 @@ export class EditFormComponent implements OnInit {
     this._snackBar.open(msg, 'Cerrar', {
       duration: 4000,
     });
+  }
+
+  subscribeSpinnerService() {
+    this.spinnerSubscription = this.spinnerService
+      .getSpinnerState()
+      .subscribe((isVisible) => {
+        this.showSpinner = isVisible;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.spinnerSubscription.unsubscribe();
   }
 }
